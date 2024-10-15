b39c1752ac4b92c328d14bdd81bfdf8cb6a12c9f
Hafidil Asad <hafidilaksad@gmail.com>
2024-10-16 05:34:33 +0700

    add register address

--- controllers/sensorController.js
+++ controllers/sensorController.js
@@ -1,11 +1,50 @@
 import client from "../config/modbusConfig.js";
 
+const dataAddress = 
+    {
+        addressLamp1:50,
+        addressLamp2:51,
+        addressLamp3:52,
+        addressLamp4:53,
+        addressLamp5:54,
+        addressLamp6:55,
+        addressLamp7:56,
+        addressLamp8:57,
+        addressLamp9:58,
+    }
+
 export const readSensor = async (req, res) => {
     try {
         const data = await client.readHoldingRegisters(
-            process.env.MODBUS_REG_START,
-            process.env.MODBUS_REG_END
+           dataAddress.addressLamp1,
+           1,
         );
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const readAllSensor = async (req, res) => {
+    try {
+        const data = await client.readHoldingRegisters(
+           dataAddress.addressLamp1,
+           10,
+        );
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const writeData = async (req, res) => {
+    try {
+         await client.writeRegister(
+           dataAddress.addressLamp1,
+           1,
+        );
+        readAllSensor();
+
         res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: error.message });
--- index.js
+++ index.js
@@ -2,21 +2,10 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import sensorRoutes from "./routes/sensorRoutes.js";
-import Stream from "node-rtsp-stream";
 
 dotenv.config();
 
 const app = express();
-const stream = new Stream({
-    name: 'name',
-    streamUrl: 'rtsp://admin:Komponen1@192.168.1.22:5543/051c6519288cc2d8b07f026902be8c96/live/channel0',
-    wsPort: 9999,
-    ffmpegOptions: { // options to pass to ffmpeg
-        '-stats': '',
-        '-r': 30,
-    },
-})
-
 
 
 app.use(cors());
